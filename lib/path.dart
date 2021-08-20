1069c7a8694d50e7b105138232087605d1a4737c
菘菘 <rrt467778@gmail.com>
2021-08-20 17:43:17 +0800

    Create path.dart

---
+++ lib/path.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+
+late var home;
+
+_getData() {
+  Map<String, String> envVars = Platform.environment;
+  if (Platform.isLinux) {
+    home = envVars['HOME'];
+    return join(home, ".local", "share");
+  } else if (Platform.isMacOS) {
+    home = envVars['HOME'];
+  } else if (Platform.isWindows) {
+    home = envVars['UserProfile'];
+    return join(home, "AppData", "Roaming");
+  }
+}
+
+_getConfig() {
+  Map<String, String> envVars = Platform.environment;
+  if (Platform.isLinux) {
+    home = envVars['HOME'];
+    return join(home, ".config");
+  } else if (Platform.isMacOS) {
+    home = envVars['HOME'];
+  } else if (Platform.isWindows) {
+    home = envVars['UserProfile'];
+    return join(home, "AppData", "Local");
+  }
+}
+
+var dataHome = Directory(join(_getData(), "RPMTranslator"));
+var configHome = Directory(join(_getConfig(), "RPMTranslator"));
