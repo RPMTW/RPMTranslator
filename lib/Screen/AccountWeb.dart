e034a042139240dfdf783927302f396534a7e8c8
菘菘 <rrt467778@gmail.com>
2021-08-26 23:46:39 +0800

    web oauth2 test

--- lib/Screen/AccountWeb.dart
+++ lib/Screen/AccountWeb.dart
@@ -45,141 +45,16 @@ class AccountWebScreen_ extends State<AccountWebScreen> {
       } else {
         return AlertDialog(
           title: Text("登入 Crowdin 帳號", textAlign: TextAlign.center),
-          content: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Crowdin 登入權杖',
-                      hintText: 'Token',
-                      prefixIcon: Icon(Icons.password)),
-                  controller: TokenController,
-                  obscureText: _obscureText // 設定控制器
-                  ),
-              TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _obscureText = !_obscureText;
-                    });
-                  },
-                  child: Text(_obscureText ? "顯示登入權杖" : "隱藏登入權杖")),
-              IconButton(
-                icon: Icon(Icons.login),
-                onPressed: () {
-                  showDialog(
-                      barrierDismissible: false,
-                      context: context,
-                      builder: (context) {
-                        return Builder(builder: (context) {
-                          if (TokenController.text == "") {
-                            return AlertDialog(
-                              title:
-                                  Text("登入權杖不能為空", textAlign: TextAlign.center),
-                              actions: [
-                                IconButton(
-                                  icon: Icon(Icons.close),
-                                  tooltip: "OK",
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                )
-                              ],
-                            );
-                          } else {
-                            return FutureBuilder(
-                                future: CrowdinAuthHandler.CheckToken(
-                                    TokenController.text),
-                                builder: (context, AsyncSnapshot snapshot) {
-                                  if (snapshot.hasData &&
-                                      snapshot.data.length > 0) {
-                                    List data = snapshot.data;
-                                    Account.add(TokenController.text, "",
-                                        data[1]['data']['id']);
-                                    return AlertDialog(
-                                        title: Text("登入成功",
-                                            textAlign: TextAlign.center),
-                                        actions: [
-                                          IconButton(
-                                            icon: Icon(Icons.close),
-                                            tooltip: "確定",
-                                            onPressed: () {
-                                              Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                      builder: (context) =>
-                                                          App()));
-                                            },
-                                          )
-                                        ]);
-                                  } else if ((snapshot.hasData &&
-                                          snapshot.data.length == 0) ||
-                                      snapshot.hasError) {
-                                    return AlertDialog(
-                                        title: Text("登入失敗",
-                                            textAlign: TextAlign.center),
-                                        content: Text(
-                                            "登入權杖錯誤，請嘗試再次登入，如果仍然無效請到 RPMTW Discord 伺服器詢問。",
-                                            textAlign: TextAlign.center),
-                                        actions: [
-                                          IconButton(
-                                            icon: Icon(Icons.refresh),
-                                            tooltip: "重新登入",
-                                            onPressed: () {
-                                              Navigator.pop(context);
-                                              Navigator.pop(context);
-                                              showDialog(
-                                                  context: context,
-                                                  builder: (context) =>
-                                                      AccountWebScreen());
-                                            },
-                                          )
-                                        ]);
-                                  } else {
-                                    return AlertDialog(
-                                      title: Text("正在偵測登入權杖是否有效，請稍後...",
-                                          textAlign: TextAlign.center),
-                                      content: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          SizedBox(
-                                            height: 12,
-                                          ),
-                                          CircularProgressIndicator(),
-                                          SizedBox(
-                                            height: 12,
-                                          ),
-                                        ],
-                                      ),
-                                    );
-                                  }
-                                });
-                          }
-                        });
-                      });
-                },
-                tooltip: "登入",
-              )
-            ],
-          ),
+          content: Text("點擊下方的登入按鈕將會開啟 Crowdin 的授權網頁，請在該網頁上登入帳號"),
           actions: [
             IconButton(
-              icon: Icon(Icons.close),
-              tooltip: "關閉介面",
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.help_outline),
-              tooltip: "如何取得登入權杖?",
+              icon: Icon(Icons.login),
               onPressed: () {
                 utility.OpenUrl(
-                    'https://www.rpmtw.ga/Wiki/ModInfo#crowdin-login-token');
+                    'https://accounts.crowdin.com/oauth/authorize?client_id=8HpxK2jINouRXTrVq6gf&redirect_uri=https://rear-end.a102009102009.repl.co/crowdin/oauth/auth/web&response_type=code&scope=project');
               },
-            )
+              tooltip: "登入",
+            ),
           ],
         );
       }
