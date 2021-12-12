6edf2920f820810e5228d3b014c189842f8c43fa
菘菘 <rrt467778@gmail.com>
2021-12-12 20:36:24 +0800

    Update APIs.dart

--- lib/API/APIs.dart
+++ lib/API/APIs.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: non_constant_identifier_names, unused_local_variable, file_names, avoid_print, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-String HomePageUrl = "https://www.rpmtw.ga";
+String HomePageUrl = "https://www.rpmtw.com";
 String RPMTWProgressAPI =
     "https://raw.githubusercontent.com/RPMTW/RPMTW-website-data/main/data/progress.json";
 String RPMTWContributionAPI = "https://raw.githubusercontent.com/RPMTW/RPMTW-website-data/main/data/opt.json";
